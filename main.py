b03319bd61c7b7a812b243332b8ab38b1be48eff
tertullus <72622240+tertullus@users.noreply.github.com>
2024-09-25 09:53:13 +0100

    Add image uploader endpoint

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
 from fastapi import FastAPI
+import json
+import cloudinary.api
+import cloudinary.uploader
+from cloudinary import CloudinaryImage
+import cloudinary
+from dotenv import load_dotenv
+load_dotenv()
+
+config = cloudinary.config(secure=True)
 
 app = FastAPI()
 
 app.username = ""
+app.image_url = ""
 
 
 @app.get("/")
 async def home():
-    return f"Welcome home, {app.username}."
+    return f"Welcome, {app.username}. Here's the image URL: {app.image_url}"
 
 
 @app.post("/login")
@@ -21,5 +31,13 @@ async def login():
 
 
 @app.delete("/reset")
-def reset():
+async def reset():
     app.username = ""
+
+
+@app.post("/upload")
+async def upload_image():
+    cloudinary.uploader.upload(
+        "me.jpg", public_id="me", unique_filename=False, overwrite=True)
+    app.image_url = CloudinaryImage("me").build_url()
+    return f"The image URL: {app.image_url}"
